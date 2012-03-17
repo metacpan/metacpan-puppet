91b5621463a9b7cfa032e9f15b30fb43f81c8a4f
Leo Lapworth <leo@cuckoo.org>
2012-03-17 18:55:20 +0000

    Some test files

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,5 +4,17 @@ $moduleserver = "puppet://localhost:9100"
 # import "modules.pp"
 # import "../classes/*.pp"
 
-
+class base_files {
+    file { "/tmp/leo_testing.txt":
+            owner => "root",
+            group => "root",
+            mode => 440,
+            source => "$fileserver/default/tmp/leo_test.txt",
+    }
+}
+node localhost {
+    # Setup all machines the same (for now at least)
+    include base_files
+    
+}
 
