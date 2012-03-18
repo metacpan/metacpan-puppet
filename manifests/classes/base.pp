3c1a78c7ed93ccb780046ee874a3104fd7bee1cd
Leo Lapworth <leo@cuckoo.org>
2012-03-18 22:26:42 +0000

    Add more packages

--- manifests/classes/base.pp
+++ manifests/classes/base.pp
@@ -24,47 +24,16 @@ class default_packages {
 	package { bzip2: ensure => present }
 	package { diffutils: ensure => present }
 
-#   package { host: ensure => present }
-#   package { htop: ensure => present }
-#   package { mutt: ensure => present }
-#   package { less: ensure => present }
-#   package { ncftp: ensure => present }
-#   package { ntp: ensure => present }
-#   package { ntpdate: ensure => present }
-#   package { psmisc: ensure => present } # killall pstree fuser commands
-#   package { rsync: ensure => present }
-#   package { screen: ensure => present }
-#   package { sudo: ensure => present }
-#   package { sysstat: ensure => present }
-#   package { telnet: ensure => present }
-#   package { whois: ensure => present }
-# 
-#   # Daemons
-#   package { memcached: ensure => present } 
-# 
-#   # Development tools
-#   package { gcc:       ensure => present }
-#   package { make:      ensure => present }
-#   package { libc6:     ensure => present }
-#   package { libc6-dev: ensure => present }
-#   package { libxml2-utils:   ensure => present }
-#   package { pkg-config: ensure => present }
-#   package { build-essential: ensure => present }
-# # for building ssh
-#   
-#       
-#   # Stuff for firewall / security
-#   package { iptables: ensure => installed }
-#   package { nmap: ensure => present }
-#   package { chkrootkit: ensure => present }
-#   package { tcpdump: ensure => present }
-#   package { fake: ensure => present }
-#   package { dnsutils: ensure => present }
-#   package { net-tools: ensure => present } 
-# 
-# # Stats / Analysis
-# package { graphviz: ensure => present } 
+    package { htop: ensure => present }
+    package { psmisc: ensure => present } # killall pstree fuser commands
+    package { rsync: ensure => present }
+    package { screen: ensure => present }
+    package { sudo: ensure => present }
+    package { sysstat: ensure => present }
+    package { whois: ensure => present }
 
-
+    # Stuff for firewall / security
+    package { iptables: ensure => installed }
+    package { chkrootkit: ensure => present }
 
 }
