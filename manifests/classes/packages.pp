6528acb511ea3449971057754a5b7ce9a2216dd5
Leo Lapworth <leo@cuckoo.org>
2012-03-18 22:35:23 +0000

    Move YUM into class

---
+++ manifests/classes/packages.pp
@@ -0,0 +1,20 @@
+class suse_yum {
+
+    Yumrepo { gpgcheck=>1, enabled=>1 }
+
+    yumrepo {
+        "SL-11.3":
+          descr=>"openSuSE Linux 11.3",
+          baseurl=>"http://download.opensuse.org/repositories/server:/http/openSUSE_11.3/";
+        "Packman-11.3":
+          descr=>"Packman repository (openSUSE_11.3)",
+          baseurl=>"http://packman.inode.at/suse/openSUSE_11.3/";
+        "YaST_Web-11.3":
+          descr=>"Web interface for YaST modules (openSUSE_11.3)",
+          baseurl=>"http://download.opensuse.org/repositories/YaST:/Web/openSUSE_11.3/";
+        "repo-oss-11.3":
+          descr=>"openSUSE-11.3-Oss",
+          baseurl=>"http://download.opensuse.org/distribution/11.3/repo/oss/suse/";
+    }
+
+}
--- manifests/site.pp
+++ manifests/site.pp
@@ -4,25 +4,13 @@ $moduleserver = "puppet://localhost"
 import "modules.pp"
 import "classes/*.pp"
 
-Yumrepo { gpgcheck=>1, enabled=>1 }
-
-yumrepo {
-    "SL-11.3":
-      descr=>"openSuSE Linux 11.3",
-      baseurl=>"http://download.opensuse.org/repositories/server:/http/openSUSE_11.3/";
-    "Packman-11.3":
-      descr=>"Packman repository (openSUSE_11.3)",
-      baseurl=>"http://packman.inode.at/suse/openSUSE_11.3/";
-    "YaST_Web-11.3":
-      descr=>"Web interface for YaST modules (openSUSE_11.3)",
-      baseurl=>"http://download.opensuse.org/repositories/YaST:/Web/openSUSE_11.3/";
-    "repo-oss-11.3":
-      descr=>"openSUSE-11.3-Oss",
-      baseurl=>"http://download.opensuse.org/distribution/11.3/repo/oss/suse/";
-}
 
 case $operatingsystem {
-  suse: { Package{ provider => yum } }
+  suse: { 
+    include suse_yum
+    Package{ provider => yum }
+    
+  }
 }
 
 node localhost {
