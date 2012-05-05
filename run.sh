65313a51418a83377267b967fbf9d359772c05be
Leo Lapworth <leo@cuckoo.org>
2012-05-05 20:56:00 +0100

    Only update the perms we care about!

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-find . -type d -exec chmod a+rx {} \;
-find . -type f -exec chmod a+r {} \;
+find /etc/puppet -type d -exec chmod a+rx {} \;
+find /etc/puppet -type f -exec chmod a+r {} \;
 
 /etc/init.d/puppetmaster start
 # cd /etc/puppet
