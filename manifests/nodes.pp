d9c3d2b203208eaa9e1e6ef693df05b5ad6588c5
root <root@metacpan-dev.localdomain>
2012-10-20 20:31:13 +0200

    move node definition in separate file

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,38 +1,4 @@
 # Setup all machines the same (for now at least)
 
 $perlbrew_ = 'export PATH=/usr/local/perlbrew/perls/metalib/bin:$PATH'
-
-node localhost {
-
-    $perlbin = '/usr/local/perlbrew/perls/metalib/bin'
-
-    # Load all the generic metacpan stuff
-    include metacpan
-
-    # Perl take quite a while, so comment out
-    # if your testing other things
-    # include metacpan::perl::modules
-
-    include metacpan::ssh::server
-
-    # Choose what we want
-    include metacpan::users::basic
-    include metacpan::users::admins
-    
-    include metacpan::website::api
-    # include metacpan::cron::api
-
-    include metacpan::website::www
-    
-    # 14336 = live elasticsearch_memory_mb
-    $elasticsearch_memory_mb = '50'
-    $es_live_version = '0.18.5'
-    include elasticsearch::install
-
-    $vhosts = [ "munin" ]
-    include nginx
-
-    include munin::web
-    include munin-server
-    
-}
+import "nodes/*.pp"
---
+++ manifests/nodes/default.pp
@@ -0,0 +1,34 @@
+node default {
+
+    $perlbin = '/usr/local/perlbrew/perls/metalib/bin'
+
+    # Load all the generic metacpan stuff
+    include metacpan
+
+    # Perl take quite a while, so comment out
+    # if your testing other things
+    # include metacpan::perl::modules
+
+    include metacpan::ssh::server
+
+    # Choose what we want
+    include metacpan::users::basic
+    include metacpan::users::admins
+
+    include metacpan::website::api
+    # include metacpan::cron::api
+
+    include metacpan::website::www
+
+    # 14336 = live elasticsearch_memory_mb
+    $elasticsearch_memory_mb = '50'
+    $es_live_version = '0.18.5'
+    include elasticsearch::install
+
+    $vhosts = [ "munin" ]
+    include nginx
+
+    include munin::web
+    include munin-server
+
+}
