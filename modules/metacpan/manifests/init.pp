f9d16797e3eaaa8a642c915ffb51fac5264d4198
Moritz Onken <onken@netcubed.de>
2012-10-21 20:06:59 +0200

    set exim type to internet

--- modules/metacpan/manifests/init.pp
+++ modules/metacpan/manifests/init.pp
@@ -4,6 +4,7 @@ class metacpan {
     metacpan::user { metacpan: }
     metacpan::perl {  $perl: }
     include metacpan::rrrclient
+    include metacpan::exim
     # Load in, some of these do stuff, some just mean you
     # can use the sub class from nodes.pp
     include metacpan::ssh
