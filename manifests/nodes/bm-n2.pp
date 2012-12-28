290009b1819dc22a2f06c72622beaaac12c62680
Moritz Onken <onken@netcubed.de>
2012-12-28 22:06:25 +0000

    enable stuff

--- manifests/nodes/bm-n2.pp
+++ manifests/nodes/bm-n2.pp
@@ -1,29 +1,20 @@
 node bm-n2 {
-
-    $perlbin = '/usr/local/perlbrew/perls/metalib/bin'
-
+    $perlbin = "/usr/local/perlbrew/perls/perl-5.16.2/bin"
     # Load all the generic metacpan stuff
-    #include metacpan
+    include nginx
+    include metacpan
 
     #include metacpan::ssh::server
 
-    # Choose what we want
-    #include metacpan::user::admins
+    include metacpan::user::admins
 
-    #include metacpan::website::api
     # include metacpan::cron::api
 
-    #include metacpan::website::www
 
-    # 14336 = live elasticsearch_memory_mb
     $elasticsearch_memory_mb = 14336
     $es_live_version = '0.20.2'
     include elasticsearch::install
 
-    #$vhosts = [ "munin" ]
-    #include nginx
-
-    # Don't enable munin on dev machines
     # include munin::web
     # include munin-server
 
