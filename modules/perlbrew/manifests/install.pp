bcf4b4d733dd77051d2b3d20c0f8396018a7bfb2
Moritz Onken <moritz.onken@siemens.com>
2012-12-28 15:08:15 -0500

    require packages

--- modules/perlbrew/manifests/install.pp
+++ modules/perlbrew/manifests/install.pp
@@ -1,19 +1,11 @@
 class perlbrew::install {
-  # package {
-  #   "build-essential":
-  #     ensure => present,
-  # }  
-  # package {
-  #   "wget":
-  #     ensure => present,
-  # }
-
   file {
     $perlbrew::params::perlbrew_bin:
       owner   => root,
       group   => root,
       mode    => 0755,
       source  => "puppet:///modules/perlbrew/perlbrew",
+      require => [Package["wget"], Package["build-essential"], Package["bzip2"]],
   }->
   file {
     $perlbrew::params::patchperl_bin:
