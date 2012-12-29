77753cdfc148ff0c5133832049589292dd67d5d5
Moritz Onken <onken@netcubed.de>
2012-12-28 21:57:25 -0500

    include munin vhost only if munin is configured

--- modules/metacpan/manifests/web.pp
+++ modules/metacpan/manifests/web.pp
@@ -3,5 +3,7 @@ class metacpan::web {
 	include metacpan::web::api
 	include metacpan::web::sco
 	include metacpan::web::cpan
-	include metacpan::web::munin
+	if defined(Class["munin-server"]) {
+		include metacpan::web::munin
+	}
 }
