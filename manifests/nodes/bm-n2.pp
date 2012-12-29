09c1ca7a627f5f3333318fb5d00262df34413751
Moritz Onken <onken@netcubed.de>
2012-12-29 11:59:57 -0500

    define munin before metacpan, fixes vhost setup

--- manifests/nodes/bm-n2.pp
+++ manifests/nodes/bm-n2.pp
@@ -1,10 +1,10 @@
 node bm-n2 {
     $perlbin = "/usr/local/perlbrew/perls/perl-5.16.2/bin"
+    include metacpan::munin
     include metacpan
     include metacpan::exim
     include metacpan::user::admins
     include metacpan::cron::api
-    include metacpan::munin
 
     elasticsearch { "0.20.2": memory  => 18000 }
 }
