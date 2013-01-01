d5e234a294174bb3beb23fc4226128300d5b962e
Moritz Onken <onken@netcubed.de>
2013-01-01 11:54:19 -0500

    enable status

--- modules/metacpan/manifests/web.pp
+++ modules/metacpan/manifests/web.pp
@@ -3,6 +3,7 @@ class metacpan::web {
 	include metacpan::web::api
 	include metacpan::web::sco
 	include metacpan::web::cpan
+	include metacpan::web::status
 	if defined(Class["munin-server"]) {
 		include metacpan::web::munin
 	}
