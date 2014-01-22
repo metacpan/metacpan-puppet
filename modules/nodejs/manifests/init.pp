d6e9fde81250ba0bb9004cb4a343097f37a14e41
Randy Stauner <randy@magnificent-tears.com>
2014-01-22 14:24:58 -0700

    Increase timeout for node.js installation

--- modules/nodejs/manifests/init.pp
+++ modules/nodejs/manifests/init.pp
@@ -3,6 +3,7 @@ class nodejs {
   exec { "nodejs":
     command => "wget http://nodejs.org/dist/v0.10.24/node-v0.10.24.tar.gz && tar -xzf node-v0.10.24.tar.gz && cd node-v0.10.24 && ./configure && make install",
     path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
+    timeout => 600, # 10 min
     cwd => "/tmp",
     onlyif => "test ! -f /usr/local/bin/node"
   }
