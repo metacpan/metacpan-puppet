3d3df80db8ac13946b4ce45991db3d25082cb9cb
Moritz Onken <onken@netcubed.de>
2014-03-20 16:21:03 +0100

    use new metacpan explorer

--- modules/metacpan/manifests/web/explorer.pp
+++ modules/metacpan/manifests/web/explorer.pp
@@ -1,19 +1,5 @@
 class metacpan::web::explorer {
 	nginx::vhost { "explorer.metacpan.org":
-		bare     => true,
-	}
-	nginx::proxy { "explorer.metacpan.org":
-		target   => "http://localhost:5002",
-		vhost    => "explorer.metacpan.org",
-		location => "",
-	}
-	startserver { "metacpan-explorer":
-		root    => "/home/metacpan/explorer.metacpan.org",
-		perlbin => $perlbin,
-        port    => 5002,
-	}->
-	service { "metacpan-explorer":
-		ensure => running,
-		enable => true,
+		root	=> "/home/metacpan/explorer.metacpan.org/build"
 	}
 }
