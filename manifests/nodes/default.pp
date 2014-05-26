47e93483f23f6b5e2abcc8c5fe8d3b76848eca44
Randy Stauner <randy@magnificent-tears.com>
2014-05-26 16:24:23 -0700

    Give es 70% of available memory in dev vm

--- manifests/nodes/default.pp
+++ manifests/nodes/default.pp
@@ -7,5 +7,10 @@ node default {
 
     include metacpan
     include metacpan::ssh
-    elasticsearch { "0.20.2": }
+    elasticsearch { "0.20.2":
+      # Give es 70% of the available memory.
+      # As of 20140526 the 'facter' installed on the vm is too old to have
+      # $memorysize_mb, so use ruby to strip the suffix.
+      memory => inline_template('<%= (scope.lookupvar("::memorysize").sub(%r/\s*MB$/, "").to_f * 0.70).to_i %>'),
+    }
 }
