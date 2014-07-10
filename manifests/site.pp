1ce657343d38a09242a1bd4eb0b1d99179166f39
Leo Lapworth <leo@cuckoo.org>
2014-07-10 21:29:34 +0100

    first bit of data via hiera

--- manifests/site.pp
+++ manifests/site.pp
@@ -30,19 +30,23 @@ node bm-n2 {
 # dev server
 
 node default {
-    $perlbin = "/usr/local/perlbrew/perls/perl-5.16.2/bin"
-    $apiworkers = 1
-    $wwwworkers = 3
 
-    include metacpan
-    include metacpan::ssh
-    elasticsearch { "0.20.2":
-      # Give es 70% of the available memory.
+$foo = hiera('wibble', 33)
+notice("The value is... ${foo}")
+
+#    $perlbin = "/usr/local/perlbrew/perls/perl-5.16.2/bin"
+#    $apiworkers = 1
+#    $wwwworkers = 3
+
+#    include metacpan
+#    include metacpan::ssh
+#    elasticsearch { "0.20.2":
+#      # Give es 70% of the available memory.
       # As of 20140526 the 'facter' installed on the vm is too old to have
       # $memorysize_mb, so use ruby to strip the suffix.
       # Transform GB to MB.
-      memory => inline_template(
-        '<%= (scope.lookupvar("::memorysize").sub(%r/^(.+?)\s*([GM])B$/){ $1.to_f * ($2 == "G" ? 1000 : 1) * 0.70 }.to_i) %>'
-      ),
-    }
+#      memory => inline_template(
+#        '<%= (scope.lookupvar("::memorysize").sub(%r/^(.+?)\s*([GM])B$/){ $1.to_f * ($2 == "G" ? 1000 : 1) * 0.70 }.to_i) %>'
+#      ),
+#    }
 }
