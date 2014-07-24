35b6f6f58823891fb58b58b4e597c6cd31febedc
Randy Stauner <randy@magnificent-tears.com>
2014-07-23 18:41:30 -0700

    Install services for Daemon::Control scripts

---
+++ modules/daemon_control/manifests/init.pp
@@ -0,0 +1,26 @@
+# Install init script for Daemon::Control wrapper.
+define daemon_control (
+    $root,
+    $service        = $name,
+    $service_enable = true,
+    $user           = 'metacpan',
+) {
+
+    $init     = "/etc/init.d/${service}"
+    $env_file = '/home/metacpan/.metacpanrc'
+    $script   = "${root}/bin/daemon-control.pl"
+
+    file { $init:
+        ensure  => file,
+        mode    => '0755',
+        owner   => 'root',
+        group   => 'root',
+        content => template('daemon_control/init.sh.erb'),
+    }
+
+    service { $service:
+        ensure  => $service_enable,
+        enable  => $service_enable,
+        require => File[$init],
+    }
+}
