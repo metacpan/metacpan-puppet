7ba36d3884b529b207bca842d8dd62154953a9a9
Leo Lapworth <leo@cuckoo.org>
2014-08-20 21:37:30 +0100

    remove "location_roots" from nginx - was not being used

--- modules/nginx/manifests/proxy.pp
+++ modules/nginx/manifests/proxy.pp
@@ -4,7 +4,6 @@ define nginx::proxy(
         $location = "/$name",
         $vhost,
         $vhost_root = "/etc/nginx/conf.d/$vhost",
-        $location_roots = undef, # template var
 ) {
         include nginx
         realize File["$vhost_root.d"]
