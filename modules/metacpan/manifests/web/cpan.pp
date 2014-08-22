a9302271bc3bd59e363c80d5be162e76c7131967
Leo Lapworth <leo@cuckoo.org>
2014-08-22 22:09:47 +0100

    convert explorer to hiera

--- modules/metacpan/manifests/web/cpan.pp
+++ modules/metacpan/manifests/web/cpan.pp
@@ -1,12 +1,11 @@
 class metacpan::web::cpan(
-	$cpan_mirror = hiera('metacpan::cpan_mirror', '/var/cpan')
 ) {
-	nginx::vhost { "cpan.metacpan.org":
-		html      => $cpan_mirror,
-		ssl       => true,
-		autoindex => true,
-		aliases   => ["cpan.$hostname.metacpan.org", "cpan.lo.metacpan.org"],
-	}
+	# nginx::vhost { "cpan.metacpan.org":
+	# 	html      => $cpan_mirror,
+	# 	ssl       => true,
+	# 	autoindex => true,
+	# 	aliases   => ["cpan.$hostname.metacpan.org", "cpan.lo.metacpan.org"],
+	# }
 
   realize File["/etc/nginx/conf.d/cpan.metacpan.org.d"]
 	file { "/etc/nginx/conf.d/cpan.metacpan.org.d/cdn-fastly.conf":
--- modules/metacpan/manifests/web/explorer.pp
+++
@@ -1,5 +0,0 @@
-class metacpan::web::explorer {
-	nginx::vhost { "explorer.metacpan.org":
-		html	=> "/home/metacpan/explorer.metacpan.org/build"
-	}
-}
