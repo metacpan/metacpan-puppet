df8dde78a01ee4ab23b25ba4f268fffecde49b39
Leo Lapworth <leo@cuckoo.org>
2014-08-22 21:17:15 +0100

    firewall rules in hiera for development

--- modules/metacpan/manifests/init.pp
+++ modules/metacpan/manifests/init.pp
@@ -8,6 +8,10 @@ class metacpan(
     $websites = hiera_hash('metacpan::websites', {})
     create_resources('metacpan::website', $websites)
 
+    # Run any fire wall stuff here
+    $fw_rules = hiera_hash('metacpan::fw_ports', {})
+    create_resources('metacpan::system::firewall', $fw_rules)
+
     metacpan::user { metacpan:
         expire_password   => false,
         source_metacpanrc => true,
---
+++ modules/metacpan/manifests/system/firewall.pp
@@ -0,0 +1,18 @@
+define metacpan::system::firewall(
+  $ensure = present,
+  $order = '300',
+  $port,
+  $proto = 'tcp',
+  $action = 'accept',
+  $source = '127.0.0.1',
+) {
+
+  firewall { "${order} ${action} ${proto} ${port} ${name}":
+      ensure  => $ensure,
+      port    => [ $port ],
+      proto   => $proto,
+      action  => $action,
+      source  => $source, # anywhere
+    }
+
+}
