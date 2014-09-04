7a4105421aad702153459004fed92326ba0fc497
Leo Lapworth <leo@cuckoo.org>
2014-09-04 19:38:27 +0100

    fixes for the name change

--- modules/metacpan/manifests/web.pp
+++ modules/metacpan/manifests/web.pp
@@ -1,7 +1,7 @@
 # Variour bits that don't fit into hiera yet
 class metacpan::web {
 
-	file { '/home/metacpan/metacpan_web/root/static/sitemaps':
+	file { '/home/metacpan/metacpan-web/root/static/sitemaps':
 			ensure => directory,
 			owner => 'metacpan',
 			group => 'metacpan',
