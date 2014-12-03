67a272ff5c0f93aa4cc5382e1c220f4b1b11efc2
Leo Lapworth <leo@cuckoo.org>
2014-12-03 21:42:33 +0000

    start bm-mc-03 setup

--- modules/facts/lib/facter/datacenter.rb
+++ modules/facts/lib/facter/datacenter.rb
@@ -2,9 +2,10 @@ Facter.add(:datacenter) do
   setcode do
     case Facter.value(:ipaddress)
     when /^5\.153\.225\./
+      # bm-mc-01 and bm-mc-02
       'bytemark-YO26-york'
     when /^46\.43\.35\./
-      # This was the old IP address for BM
+      # bm-mc-03
       'bytemark-YO26-york'
     when /^50\.28\.18\./
       'liquidweb'
