1209f96bb9accc53b33d23aa76bc32c01e77c75b
Matt S Trout <mst@shadowcat.co.uk>
2016-04-23 17:44:12 +0000

    first attempt at a class for installing postgres but leaving it stopped

---
+++ modules/metacpan_postgres/manifests/install_only.pp
@@ -0,0 +1,13 @@
+class metacpan_postgres::install_only(
+  ) {
+
+    # Install postgres
+    class { 'postgresql::globals':
+                 manage_package_repo => true,
+                 version             => '9.5',
+        }->class { 'postgresql::server':
+          listen_addresses => '*',
+          service_ensure => 'stopped',
+          service_enable => 'false',
+        }
+}
