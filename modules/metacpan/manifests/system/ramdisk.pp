05896808217889731d3c60ecf9b6ed1f87fba470
Leo Lapworth <leo@cuckoo.org>
2016-05-20 22:07:09 +0100

    add cron job to clear out content in ramdisk

--- modules/metacpan/manifests/system/ramdisk.pp
+++ modules/metacpan/manifests/system/ramdisk.pp
@@ -1,5 +1,6 @@
 define metacpan::system::ramdisk (
     $size,
+    $valid_mins = 0,
     $owner = hiera('metacpan::user', 'metacpan'),
     $group = hiera('metacpan::group', 'metacpan'),
 ) {
@@ -20,4 +21,30 @@ define metacpan::system::ramdisk (
     pass    => 0,
     require => File[$name],
   }
+
+  case $valid_mins {
+    0: {
+      $enable_cron = absent
+    }
+    default: {
+      $enable_cron = present
+    }
+
+  }
+
+  $cmd = "find ${name} -maxdepth 1 -mindepth 1 -mmin +${valid_mins} -exec rm -rf {} \;"
+
+  cron {
+    "cron_${name}":
+        user        => $owner,
+        command     => $cmd,
+        environment => [
+          # On start-stop-daemon is in /sbin.
+          'PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+        ],
+        hour        => '*',
+        minute      => '*/5',
+        ensure      => $enable_cron
+  }
+
 }
