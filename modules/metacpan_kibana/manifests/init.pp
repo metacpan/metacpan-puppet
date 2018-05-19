07c29a02958c3d28e71eba2ae3908eac657cfd5c
Leo Lapworth <leo@cuckoo.org>
2018-05-19 19:53:56 +0100

    pin kibana version

--- modules/metacpan_kibana/manifests/init.pp
+++ modules/metacpan_kibana/manifests/init.pp
@@ -2,6 +2,15 @@ class metacpan_kibana() {
 
   include metacpan_kibana::package
 
+    apt::pin { 'kibana':
+        version   => '4.6.3',
+        priority  => 1001,
+        packages  => 'kibana'
+    }
+
+
+  Unpacking kibana (6.2.4) over (4.6.3) ...
+
   service { "kibana":
     ensure     => 'running',
     require => Exec['kibana_deb_install'],
