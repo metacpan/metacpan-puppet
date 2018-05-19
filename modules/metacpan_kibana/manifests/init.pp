c6572df6c297ab4ddb25ddcaa09894acb525e585
Leo Lapworth <leo@cuckoo.org>
2018-05-19 20:09:04 +0100

    silly copy/paste

--- modules/metacpan_kibana/manifests/init.pp
+++ modules/metacpan_kibana/manifests/init.pp
@@ -2,14 +2,11 @@ class metacpan_kibana() {
 
   include metacpan_kibana::package
 
-    apt::pin { 'kibana':
-        version   => '4.6.3',
-        priority  => 1001,
-        packages  => 'kibana'
-    }
-
-
-  Unpacking kibana (6.2.4) over (4.6.3) ...
+  apt::pin { 'kibana':
+      version   => '4.6.3',
+      priority  => 1001,
+      packages  => 'kibana'
+  }
 
   service { "kibana":
     ensure     => 'running',
