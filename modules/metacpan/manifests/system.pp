aa4acc89dadfc53733604fbcdbcf4bd94e56d3a9
Leo Lapworth <leo@cuckoo.org>
2018-05-19 22:16:51 +0100

    install backport access

--- modules/metacpan/manifests/system.pp
+++ modules/metacpan/manifests/system.pp
@@ -3,6 +3,11 @@ class metacpan::system(
 
   include apt
 
+  class { 'apt::backports':
+    # This is lower than the normal default of 500, so packages with ensure => latest don't get upgraded from backports without your explicit permission.
+    pin => 200,
+  }
+
   # Set all servers to London timezone
   class { 'timezone':
     region   => 'Europe',
