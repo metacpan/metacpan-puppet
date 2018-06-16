da55cb4f2e8ddd5dc5ca7e377fcb726299c2637f
Leo Lapworth <leo@cuckoo.org>
2018-06-16 20:32:17 +0100

    make sure 04 looks like a bytemark box in the facts

--- modules/facts/lib/facter/datacenter.rb
+++ modules/facts/lib/facter/datacenter.rb
@@ -4,8 +4,11 @@ Facter.add(:datacenter) do
     when /^5\.153\.225\./
       # bm-mc-01 and bm-mc-02
       'bytemark-YO26-york'
-    when /^46\.43\.35\./
+    when /^46\.43\.35\.68/
       # bm-mc-03
+      'bytemark-YO26-york'
+    when /^89\.16\.178\.21/
+      # bm-mc-04
       'bytemark-YO26-york'
     when /^50\.28\.18\./
       'liquidweb'
