4d7386b8ef62b95fe0c6044b3715b47834bbc3f9
Brad Lhotsky <brad@divisionbyzero.net>
2019-12-07 10:52:48 -0800

    This doesn't work exactly right
    For some reason, the `default_ipaddress` fact shows as loaded in Puppet
    before the `hieradata` is evaluated, but the value is always empty.
    Revert that back to the `ipaddress` fact.

--- modules/facts/lib/facter/ipaddress.rb
+++ modules/facts/lib/facter/ipaddress.rb
@@ -1,38 +1,11 @@
-default_interface = nil
-ipaddress = nil
-
-if File.exist? '/sbin/ip'
-  interfaces = Facter::Core::Execution.exec('/sbin/ip route show')
-
-  if interfaces
-      interfaces.split(/\n+/).each do |inf|
-      if inf.match(/^default/)
-        default_interface = inf.match(/dev (\w+)/).captures[0]
-      end
-    end
-  end
-
-  if default_interface
-    lines = Facter::Core::Execution.exec("/sbin/ip addr show dev #{default_interface}")
-    lines.split(/\n+/).each do |line|
-      line.strip!
-
-      if line.match(/^inet .*#{default_interface}/)
-        ipaddress = line.split()[1].split('/')[0]
-      end
-    end
-  end
-
-end
-
-Facter.add("default_interface") do
-  setcode do
-    default_interface
-  end
-end
-
 Facter.add("default_ipaddress") do
   setcode do
-    ipaddress
+    if Facter.value('ipaddress_eth0')
+      Facter.value('ipaddress_eth0')
+    elsif Facter.value('ipaddress_bond0')
+      Facter.value('ipaddress_bond0')
+    else
+      Facter.value('ipaddress')
+    end
   end
 end
