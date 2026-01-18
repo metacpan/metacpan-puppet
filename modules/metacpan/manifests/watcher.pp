a5369d01544cfa0add663e46e580b7d482076613
Graham Knop <haarg@haarg.org>
2026-01-18 02:11:36 +0100

    convert metacpan-watcher to run using a systemd unit
    Our servers use systemd, so we can use it for running services rather
    than using init scripts. systemd units are simpler to configure, and
    have many more easily accessible options.
    
    In particular, we can use the Restart option to automatically restart
    the watcher if it crashes.
    
    Removing the old service is a bit weird but only really has to work
    once.

--- modules/metacpan/manifests/watcher.pp
+++ modules/metacpan/manifests/watcher.pp
@@ -1,36 +1,46 @@
 class metacpan::watcher(
-	$enable = hiera('metacpan::watcher::enable', 'false'),
-	$user = hiera('metacpan::user','metacpan')
+    $enable = hiera('metacpan::watcher::enable', 'false'),
+    $user = hiera('metacpan::user','metacpan')
 ) {
-	include perl
+    include perl
 
-	# For the template
-	$filename = "metacpan-watcher"
-	$description = "Watcher script for PAUSE uploads"
-	$app_root = "/home/${user}/metacpan-api"
-	$perlbin = $perl::params::bin_dir
+    # For the template
+    $filename = "metacpan-watcher"
+    $description = "Watcher script for PAUSE uploads"
+    $app_root = "/home/${user}/metacpan-api"
+    $perlbin = $perl::params::bin_dir
+    $init     = "/etc/init.d/metacpan-watcher"
+    $unitfile = "/lib/systemd/system/metacpan-watcher.service"
 
-	$init     = "/etc/init.d/metacpan-watcher"
+    file { $init:
+        ensure => absent,
+    }
+    ~> exec { 'metacpan-watcher-init-systemd-reload':
+        command     => 'systemctl daemon-reload',
+        path        => [ '/usr/bin', '/bin', '/usr/sbin' ],
+    }
+    ~> exec { 'metacpan-watcher-init-stop':
+        command     => 'systemctl stop metacpan-watcher',
+        path        => [ '/usr/bin', '/bin', '/usr/sbin' ],
+    }
 
-	file { $init:
-		content => template("metacpan/init_watcher.erb"),
-		mode    => '0755',
-	}
+    file { $unitfile:
+        mode    => '0644',
+        owner   => 'root',
+        group   => 'root',
+        content => template("metacpan/metacpan-watcher.service.erb"),
+    }
+    ~> exec { 'metacpan-watcher-systemd-reload':
+        command     => 'systemctl daemon-reload',
+        path        => [ '/usr/bin', '/bin', '/usr/sbin' ],
+    }
 
-	service { "metacpan-watcher":
-		ensure => $enable ? {
-				true => running,
-				false => stopped,
-		},
-		enable => $enable,
-		require => [ File[$init] ],
-	}
-
-	# if( $enable == true ) {
-	#     # Everytime git code updates, restart to get new data
-	#     exec { 'restart_watcher':
-	#         command => "${init} restart",
-	#         require => [ Service['metacpan-watcher'] ],
-	#     }
-	# }
+    service { "metacpan-watcher":
+        ensure => $enable ? {
+            true => running,
+            false => stopped,
+        },
+        enable => $enable,
+        require => [ File[$unitfile] ],
+    }
 }
