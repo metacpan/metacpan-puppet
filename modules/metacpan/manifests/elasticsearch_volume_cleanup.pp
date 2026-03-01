dce705d82c782faf12bb215f950eab1db1d8553d
Olaf Alders <olaf@wundersolutions.com>
2026-03-01 15:16:06 -0500

    remove the elasticsearch LV (handles both vg-ssds and vg-hdds)

--- modules/metacpan/manifests/elasticsearch_volume_cleanup.pp
+++ modules/metacpan/manifests/elasticsearch_volume_cleanup.pp
@@ -4,9 +4,15 @@ class metacpan::elasticsearch_volume_cleanup {
     ensure => absent,
   }
 
-  exec { 'remove-elasticsearch-lv':
+  exec { 'remove-elasticsearch-lv-ssds':
     command => '/sbin/lvremove -f /dev/vg-ssds/elasticsearch',
     onlyif  => '/sbin/lvdisplay /dev/vg-ssds/elasticsearch',
+    require => Mount['/mnt/lv-elasticsearch'],
+  }
+
+  exec { 'remove-elasticsearch-lv-hdds':
+    command => '/sbin/lvremove -f /dev/vg-hdds/elasticsearch',
+    onlyif  => '/sbin/lvdisplay /dev/vg-hdds/elasticsearch',
     require => Mount['/mnt/lv-elasticsearch'],
   }
 }
